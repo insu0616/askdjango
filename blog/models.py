3f2f32033d88c74ea50f7bfd8663ae12e1fe612f
insoo <inu0616@gmail.com>
2016-04-16 13:06:04 +0900

    Azureproject Second Commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    photo =  models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
--- djangoazure/urls.py
+++ djangoazure/urls.py
@@ -1,23 +1,10 @@
-"""djangoazure URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
+from blog import views
 
 def hello(request):
     return HttpResponse('''
@@ -27,6 +14,8 @@ def hello(request):
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$' , hello),
+    url(r'^blog/$', views.post_list),
+    url(r'^blog/(?P<pk>\d+)/$', views.post_detail),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
